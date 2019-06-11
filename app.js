469d22c137ade325f3760861f0dd829db75fb45b
badri720 <badrikavteladze8@gmail.com>
2019-06-11 13:11:44 +0400

    Second

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+require('./config/passport')(passport);
+
+const auth = require('./routes/auth');
 
 const app = express();
-
-const port = process.env.PORT || 5000;
-
 
 app.get('/', (req, res) => {
 	res.send('It works!!!');
 });
 
+app.use('/auth', auth);
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
 	console.log('Server Started!!!');
---
+++ config/keys.js
@@ -0,0 +1,6 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://Badri888:bostur15@finalcluster-zfetu.mongodb.net/test?retryWrites=true&w=majority',
+	googleClientID: '697606942497-17jm5aeuadu745dtoksbh0jsilo8at9b.apps.googleusercontent.com',
+	googleClientSecret: 'JlNbhsksYbOanB5gDUIkRoyc'
+}
+
